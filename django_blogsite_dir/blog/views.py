1543b905b7ef33148588882a8f3c327b3fae86f1
Gerardo Ruiz <grville93@gmail.com>
2024-10-22 17:44:14 -0500

    Changes:
    	1. Blog app directory (with all its respective files) added to the project.
    		- This app will be the front of the whole website.
    	2. blog 'home' and blog 'about' functions added to views.py file
    	   to hande those routes.
    	3. The Project urls.py file was modified to route directly to
    	   the Blog app's urls.py file and the routes for 'home' and
    	   'about' were added to that urls.py file.

---
+++ django_blogsite_dir/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+
+
+def home(request: HttpRequest) -> HttpResponse:
+
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+
+def about(request: HttpRequest) -> HttpResponse:
+
+    return HttpResponse('<h1>Blog About</h1>')
+
+
+
+
+
+
+
+
+
+
+
+
