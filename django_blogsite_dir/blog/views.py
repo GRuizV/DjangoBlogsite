90762dc06f5b4c3b94a7b6b916ccefb1a13d5acf
Gerardo Ruiz <grville93@gmail.com>
2024-10-24 18:50:12 -0500

    Changes
    - 'Post' model added to models.py and to the database (db.sqlite3).
    
    - Actual posts from the DB are being passed to the home view in the views.py.
    
    - Post class registered in the admin.py file.

--- django_blogsite_dir/blog/views.py
+++ django_blogsite_dir/blog/views.py
@@ -1,30 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from .models import Post
 
-
-
-# Dummy data
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Aug 27, 2018'
-    },
-
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Aug 28, 2018'
-    }
-]
 
 
 def home(request: HttpRequest) -> HttpResponse:
 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context=context)
