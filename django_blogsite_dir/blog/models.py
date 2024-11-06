31affcd4a77f6db59fddb9567dee2bb09536f013
erfanmohamadnabi <erfan.web8787@gmail.com>
2024-11-06 04:38:28 +0330

    add Blog Detail View & Urls

--- django_blogsite_dir/blog/models.py
+++ django_blogsite_dir/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -10,5 +11,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # Blog Detail Url
+
+    def get_absolute_url(self):
+        return reverse("blog-detail",args=[self.title])
 
 
--- django_blogsite_dir/blog/views.py
+++ django_blogsite_dir/blog/views.py
@@ -19,9 +19,15 @@ def about(request: HttpRequest) -> HttpResponse:
     return render(request, 'blog/about.html', context = {'title': 'About'})
 
 
+# Blog Detail View
 
+def Blog_Detail(request,pk):
+    blog = Post.objects.get(id = pk)
+    context = {
+        "blog" : blog
+    }
 
-
+    return render(request,"enter file name.html",context)
 
 
 
