95640fa71540867bdf40e1f80feb4f05c245e06a
erfanmohamadnabi <erfan.web8787@gmail.com>
2024-11-06 04:53:28 +0330

    add an ckeditor to the blog

--- django_blogsite_dir/blog/models.py
+++ django_blogsite_dir/blog/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = RichTextField(verbose_name="content")
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # 'on_delete=models.CASCADE' means 'if an user is deleted, all its post too'
 
