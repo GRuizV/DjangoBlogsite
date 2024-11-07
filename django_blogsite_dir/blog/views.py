b3aad5649315cd71199978200591a4b94b5e066f
Gerardo Ruiz <grville93@gmail.com>
2024-11-06 21:03:54 -0500

    Views in Blog's app views.py now includes:
    + Class-based view named 'PostListView' that will replace the function view 'home'.
    + Class-based view named 'PostDetailView' to access individual posts.
    + Class-based view named 'PostCreateView' to create new posts from the frontend.
    + Class-based view named 'PostUpdateView' to update an already created post.
    + Class-based view named 'PostDeleteView' to delete an already created post.

--- django_blogsite_dir/blog/views.py
+++ django_blogsite_dir/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse, HttpRequest
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -11,6 +13,83 @@ def home(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, 'blog/home.html', context=context)
+
+
+
+class PostListView(ListView):
+
+    model = Post
+    template_name = 'blog/home.html'    #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+
+class PostDetailView(DetailView):
+
+    model = Post
+
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+
+    model = Post
+    fields = ['title', 'content']
+    
+    # form valid function overridden to set the author to the logged user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+
+    model = Post
+    fields = ['title', 'content']
+        
+    # form valid function overridden to set the author to the logged user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+    def test_func(self):
+
+        """
+        This is the test to make sure the author of the post to be updated
+        is the same person trying to update the post
+        """
+        
+        post = self.get_object()
+
+        if self.request.user == post.author:
+            return True
+
+        return False
+
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+
+    model = Post
+    success_url = '/'   # This is the way to redirecto to home
+
+    def test_func(self):
+
+        """
+        This is the test to make sure the author of the post to be updated
+        is the same person trying to update the post
+        """
+        
+        post = self.get_object()
+
+        if self.request.user == post.author:
+            return True
+
+        return False
+
+
 
 
 
