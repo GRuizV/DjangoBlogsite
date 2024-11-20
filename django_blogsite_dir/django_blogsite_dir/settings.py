781029f4b5665ada9178f44a060282b1170cc9cb
Gerardo Ruiz <grville93@gmail.com>
2024-11-19 23:06:37 -0500

    settings.py file included the email hosting and settings configuration to be able to send mails when a user request password resetting.

--- django_blogsite_dir/django_blogsite_dir/settings.py
+++ django_blogsite_dir/django_blogsite_dir/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,6 +145,23 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4' # Crispy form setting for more recent bootst
 
 LOGIN_REDIRECT_URL = 'blog-home' # Self-explanatory
 LOGIN_URL = 'login' # Self-explanatory
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+
+
+
+
+
+
+
 
 
 
