fd54078356c7535340d304742d19fd1cda697aab
Gerardo Ruiz <grville93@gmail.com>
2024-12-05 17:21:15 -0500

    In the project's settings.py file the secret key variable was modified to be handled as an environment variable for security reasons

--- django_blogsite_dir/django_blogsite_dir/settings.py
+++ django_blogsite_dir/django_blogsite_dir/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ycy16u&nl&nm50p!3^ep9u1s!^i3umordh52n(9dh)f)(6kda'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
