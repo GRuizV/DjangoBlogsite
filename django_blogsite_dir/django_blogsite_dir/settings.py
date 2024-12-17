4f012694e5d4fd0ccd1b5c0c57763d2366a155a4
Gerardo Ruiz <grville93@gmail.com>
2024-12-17 12:19:32 -0500

    Fix .env path for decouple

--- django_blogsite_dir/django_blogsite_dir/settings.py
+++ django_blogsite_dir/django_blogsite_dir/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+
+# Specify the path to the .env file
+env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../.env')
+config = Config(RepositoryEnv(env_path))
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
