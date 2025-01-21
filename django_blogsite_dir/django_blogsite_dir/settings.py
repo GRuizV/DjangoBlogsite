fd3d1d2a896f9760247567bf38df7dcc8b0722e0
GRuizV <grville93@gmail.com>
2025-01-21 02:31:49 +0000

    Save changes before pulling from GitHub

--- django_blogsite_dir/django_blogsite_dir/settings.py
+++ django_blogsite_dir/django_blogsite_dir/settings.py
@@ -31,7 +31,7 @@ config = Config(RepositoryEnv(env_path))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gruizv.pythonanywhere.com', 'localhost', '127.0.0.1'] # Modified to be hosted at PythonAnywhere and to be deployed locally
 
